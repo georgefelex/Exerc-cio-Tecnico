20e9e225c67c7532253d985f99eeb946f4ee8a1f
georgefelex <83044855+georgefelex@users.noreply.github.com>
2021-05-20 11:03:55 -0300

    Add files via upload

---
+++ LuxFacta.java
@@ -0,0 +1,45 @@
+package br.com.luxfacta;
+
+/*******************************************************************************
+  A classe LuxFacta recebe um parâmetro no seu construtor que é sempre um número
+   inteiro.
+  Com base nesse numero n, escreva o corpo do método say, de forma que:
+    - Se o número n for divisível por 3, o retorno deve ser "Lux"
+    - Se o número n for divisível por 5, o retorno deve ser "Facta"
+    - Se o número n for divisível por 3 e por 5, o retorno deve ser "LuxFacta"
+    - Qualquer outra condição o retorno deve ser o próprio número n;
+*******************************************************************************/
+
+public class LuxFacta {
+  private int n;
+
+  public LuxFacta(int n) {
+    this.n = n;
+  }
+
+  public String say() {
+	  
+	  //utilizei operadores logicos para caso o numero seja divisivel por 3 ou 5
+	  
+		boolean divisivelPorTres = (n%3 == 0);
+	    boolean divisivelPorCinco =  (n%5 == 0);
+	        
+	    if(divisivelPorTres && divisivelPorCinco) {
+	    	System.out.println(n);
+	    	//E utilizei operado logicos assim fiz, caso n�o seja divisivel por 3 mas e divisivel por 5
+	    }else if (divisivelPorTres == false && divisivelPorCinco == true) {
+	    	System.out.println("Lux");
+	    	//E utilizei operado logicos assim fiz, caso n�o seja divisivel por 5 mas e divisivel por 3
+	    }else if(divisivelPorCinco == false && divisivelPorTres == true) {
+	    	System.out.println("Facto");
+	    	//Caso n�o seja divisivel nem por 3 nem por 5 imprimisse LuxFactor 
+	    }else if(divisivelPorTres == false && divisivelPorCinco == false) {
+	    	System.out.println("LuxFacto");
+	    }else if(divisivelPorTres) {
+	    	System.out.println(n);	
+	    }else if(divisivelPorCinco) {
+	    	System.out.println(n);
+	    }
+    return "n";
+  }
+}
---
+++ Palindrome.java
@@ -0,0 +1,43 @@
+package br.com.luxfacta;
+
+import javax.swing.JOptionPane;
+
+/*******************************************************************************
+  Uma palÃ­ndroma Ã© uma palavra que se lÃª da mesma forma de trÃ¡s para frente.
+
+  Escreva o corpo do mÃ©todo isPalindrome que verifica se uma palavra Ã©
+  palÃ­ndroma. A validaÃ§Ã£o deve desconsiderar diferenÃ§a entre maiÃºsculas e
+  minÃºsculas (case insensitive).
+
+  Por exemplo, isPalindrome(â€œDeleveledâ€�) deve retornar true.
+*******************************************************************************/
+
+public class Palindrome {
+  public static boolean isPalindrome(String word) {
+		     String palavra, palavraInvertida="";
+		      int tamanho,i;
+
+
+		      // Armazena a palavra numa String
+		      palavra = JOptionPane.showInputDialog(null,"Digite uma palavra"); 
+
+
+		      //tamanho recebe o qtd de letras q possui a palavra
+		      tamanho=palavra.length(); 
+		      
+		      
+		      //pega a ultima letra da variavel palavra, e inseri na primeira de palavraInvertida, depois pega penultima e inseri na 2ª, e assim em diante, até pegar a primeira, e colocar na ultima.
+		      for(i=tamanho-1;i>=0;i--){  
+		          palavraInvertida += palavra.charAt(i);
+		       }
+		       
+
+		      //Compara as Duas Strings e Exibe o Resultado
+		      if(palavra.equalsIgnoreCase(palavraInvertida)) 
+		           JOptionPane.showMessageDialog(null,palavra + " É uma palavra palindroma"); 
+		      else 
+		           JOptionPane.showMessageDialog(null,palavra + " Não é uma palavra palindroma");
+		    
+    return true; 
+  }
+}
